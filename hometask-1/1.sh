4c046714cbb374e4b7867517ff3f61b90e06b6ba
Dilyara Khannanova <48886147+Dilyarem@users.noreply.github.com>
2021-12-14 14:25:36 +0300

    Delete hometask-1 directory

--- hometask-1/1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dir="$(dirname $0)/$(echo $1 | grep -o '[^\/]*' | tail -1)"
-rm -f -r $dir
-git clone $1 $dir
-cd $dir
-git diff --name-only $2..$3 > "../diff.txt"
-if ! [  $? -eq 0 ]; then
-	echo "NO SUCH BRANCHES"
-fi
